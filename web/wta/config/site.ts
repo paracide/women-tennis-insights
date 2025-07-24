b976879266dc1def875883dfee1c723393c20c34
Arthur <paracidex@gmail.com>
2025-07-23 21:54:46 -0500

    remove heroui demo

--- web/wta/config/site.ts
+++ web/wta/config/site.ts
@@ -7,23 +7,7 @@ export const siteConfig = {
     {
       label: "Home",
       href: "/",
-    },
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
+    }
   ],
   navMenuItems: [
     {
